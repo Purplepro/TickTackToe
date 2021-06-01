c76c6a17073b09ea8303ad4ad6e66dd9d7bf982f
Sebastian G <gayle054@gmail.com>
2021-06-01 06:03:36 -0700

    tictactoe

---
+++ ticktacktoe/src/App.js
@@ -0,0 +1,24 @@
+import Game from './Components/Game';
+import Board from './Components/Board';
+import Square from './Components/Board';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Board/>
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+// ========================================
+
+
